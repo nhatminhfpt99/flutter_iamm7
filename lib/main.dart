357a1545fc11657487c2df41778b60ad4528ecf0
Seven <nhatminhfpt99@gmail.com>
2021-08-30 08:36:44 +0700

    Commit
    Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            // image: NetworkImage(
+            //     'https://kenh14cdn.com/2020/10/3/rose-bi-quyet-so-huu-vong-eo-con-kien-16016906513292077166174.jpg'),
+          image: AssetImage('images/EagleM.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
