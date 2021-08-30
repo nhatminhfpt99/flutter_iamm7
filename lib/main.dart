3c48c25834dd7fcb2435e5ccc7d7418864916eca
Seven <nhatminhfpt99@gmail.com>
2021-08-30 08:38:22 +0700

    Commit
    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
           child: Image(
             // image: NetworkImage(
             //     'https://kenh14cdn.com/2020/10/3/rose-bi-quyet-so-huu-vong-eo-con-kien-16016906513292077166174.jpg'),
-          image: AssetImage('images/EagleM.png'),
+          image: AssetImage('images/EagleM7.png'),
           ),
         ),
       ),
